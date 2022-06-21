90d38e5a16de77cd472f54c7b02ee53775200cc7
Admin <Admin@SAKSHI-OSWAL>
2022-06-22 03:22:33 +0530

    test commit

--- Android Application/Bus Schedule/lib/helpers/constants.dart
+++ Android Application/Bus Schedule/lib/helpers/constants.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
 
-const kSendButtonTextStyle = TextStyle(
-  color: Colors.lightBlueAccent,
-  fontWeight: FontWeight.bold,
-  fontSize: 18.0,
-);
-
 const kMessageTextFieldDecoration = InputDecoration(
   contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
   hintText: 'Type your message here...',
