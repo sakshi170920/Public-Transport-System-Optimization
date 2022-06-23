d7e5bfbf33141a717b652d71e0d2c92775804f9f
tarun42 <tarun.medtiya18@vit.edu>
2022-06-24 01:00:19 +0530

    addon for bus list

--- Android Application/Bus Schedule/lib/screens/bus_list.dart
+++ Android Application/Bus Schedule/lib/screens/bus_list.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -15,19 +17,61 @@ class BusListView extends StatefulWidget {
 class _BusListViewState extends State<BusListView> {
   late String source;
   late String destination;
+  bool isValidInput = true;
+  int sourceInd = 0;
+  int destinationInd = 0;
   Map data = {};
+  Map indexingOfData = {};
   Future<String> getData() async {
     String url =
-        "http://ec2-44-201-223-168.compute-1.amazonaws.com:4000/getAllCities";
+        "http://ec2-44-201-223-168.compute-1.amazonaws.com:4000/getBusesBySrcDest?src=" +
+            source.replaceAll("\"", "").toLowerCase() +
+            "&dest=" +
+            destination.replaceAll("\"", "").toLowerCase();
     final response = await http.get(Uri.parse(url));
 
     this.setState(() {
-      data = jsonDecode(response.body);
-      // for (i,j) in temp;
-      // print(temp);
+      print(url);
+      try {
+        data = jsonDecode(response.body);
+      } catch (error) {
+        String apiData = response.body;
+        if (apiData == "Invalid") {
+          isValidInput = false;
+        }
+        print(apiData);
+      }
+      print(data);
 
-      // print(temp['0']);
-      // print(temp['0'][0]);
+      int ind = 0;
+      for (var i in data.keys) {
+        indexingOfData[ind] = i;
+        ind++;
+      }
+      for (var i in data.keys) {
+        for (int j = 0; j < data[i].length; j++) {
+          // print(data[i][j]);
+          if (data[i][j] is String) {
+            continue;
+          }
+          print(data[i][j][0]);
+          if (data[i][j][0].toLowerCase() == source) {
+            // print("worked");
+            sourceInd = j;
+            break;
+          }
+        }
+
+        for (int j = 0; j < data[i].length; j++) {
+          if (data[i][j] is String) {
+            continue;
+          }
+          if (data[i][j][0].toLowerCase() == destination) {
+            destinationInd = j;
+            break;
+          }
+        }
+      }
     });
     return "Success!";
   }
@@ -37,6 +81,10 @@ class _BusListViewState extends State<BusListView> {
     super.initState();
     source = getSourceAndDestination('source');
     destination = getSourceAndDestination('destination');
+    source = "hingoli";
+    destination = "sangli";
+
+    /// this need to removed
     this.getData();
   }
 
@@ -50,157 +98,155 @@ class _BusListViewState extends State<BusListView> {
                 backgroundImage: AssetImage('assets/image/person.jpg')),
           ],
         ),
-        body: ListView.builder(
-            itemCount: data == null ? 0 : data.length,
-            itemBuilder: (BuildContext cxt, int index) {
-              return Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                child: Container(
-                  padding: const EdgeInsets.all(12),
-                  decoration: BoxDecoration(
-                      color: Colors.blue[700],
-                      borderRadius: BorderRadius.circular(12)),
-                  width: double.infinity,
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: isValidInput
+            ? ListView.builder(
+                itemCount: data == null ? 0 : data.length,
+                itemBuilder: (BuildContext cxt, int index) {
+                  return Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                    child: Container(
+                      padding: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                          color: Colors.blue[700],
+                          borderRadius: BorderRadius.circular(12)),
+                      width: double.infinity,
+                      child: Column(
                         children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              const Text(
-                                'SOURCE',
-                                style: TextStyle(
-                                    fontSize: 12, color: Colors.white),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    'SOURCE',
+                                    style: TextStyle(
+                                        fontSize: 12, color: Colors.white),
+                                  ),
+                                  Text(
+                                    source.replaceAll("\"", "").toUpperCase(),
+                                    style: const TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
                               ),
-                              Text(
-                                source.replaceAll(
-                                    "\"", ""), // data['src_name']!,
-                                style: const TextStyle(
-                                    fontSize: 14,
+                              const Text(
+                                '→',
+                                style: TextStyle(
                                     color: Colors.white,
-                                    fontWeight: FontWeight.bold),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 24),
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  const Text(
+                                    'DESTINATION',
+                                    style: TextStyle(
+                                        fontSize: 12, color: Colors.white),
+                                  ),
+                                  Text(
+                                    destination
+                                        .replaceAll("\"", "")
+                                        .toUpperCase(),
+                                    style: const TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
                               ),
                             ],
-                          ),
-                          const Text(
-                            '→',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 24),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              const Text(
-                                'DESTINATION',
-                                style: TextStyle(
-                                    fontSize: 12, color: Colors.white),
-                              ),
-                              Text(
-                                destination.replaceAll(
-                                    "\"", ""), // data['des_name']!,
-                                style: const TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      Row(
-                        children: [
-                          const Icon(
-                            Icons.directions,
-                            color: Colors.white,
                           ),
                           const SizedBox(
-                            width: 5,
+                            height: 8,
                           ),
-                          Expanded(
-                            child: Text(
-                              // data['distance'].toString() +
-                              // ' KM, via ' +
-                              // data['inter_towns']!,
-                              " 258KM ,via town",
-                              maxLines: 2,
-                              // overflow: TextOverflow.ellipsis,
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.directions,
+                                color: Colors.white,
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Expanded(
+                                child: Text(
+                                  " 258KM ,via " +
+                                      data[indexingOfData[index]]
+                                              [destinationInd - 1]
+                                          [0], // hear need to add kilometer
+                                  maxLines: 2,
 
-                              style: const TextStyle(
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.directions_bus,
                                 color: Colors.white,
                               ),
-                            ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                "JBCL, 50 seater, AC",
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.access_time_rounded,
+                                color: Colors.white,
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                // data['start_time'].toString(),
+                                data[indexingOfData[index]][0],
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                              const SizedBox(
+                                width: 16,
+                              ),
+                              const Icon(
+                                Icons.timelapse_rounded,
+                                color: Colors.white,
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                // data['duration'].toString(),
+                                "duration",
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Row(
-                        children: [
-                          const Icon(
-                            Icons.directions_bus,
-                            color: Colors.white,
-                          ),
-                          const SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            // data['brand']! +
-                            // ', ' +
-                            // data['chair_count'].toString() +
-                            // ' seater, ' +
-                            // ['Non-AC', 'AC'][data['ac']!],
-                            "JBCL, 50 seater, AC",
-                            style: const TextStyle(color: Colors.white),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Row(
-                        children: [
-                          const Icon(
-                            Icons.access_time_rounded,
-                            color: Colors.white,
-                          ),
-                          const SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            // data['start_time'].toString(),
-                            "start time",
-                            style: const TextStyle(color: Colors.white),
-                          ),
-                          const SizedBox(
-                            width: 16,
-                          ),
-                          const Icon(
-                            Icons.timelapse_rounded,
-                            color: Colors.white,
-                          ),
-                          const SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            // data['duration'].toString(),
-                            "duration",
-                            style: const TextStyle(color: Colors.white),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            }));
+                    ),
+                  );
+                })
+            : Center(
+                child: Text("Oops sorry we dont have route for this cities"),
+              ));
   }
 }
